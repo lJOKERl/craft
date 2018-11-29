aabd696aa6fcb1a9bcc9a3590e990466ecf57573
ljokerl <myweb.joker@gmail.com>
2018-11-29 16:20:15 +0300

    first

---
+++ craft/script/script.js
@@ -0,0 +1,21 @@
+new Vue({
+  el: "#homepage",
+  data: {
+    showMenu: false,
+    showShed: false,
+    showPound: false,
+    showModal: false,
+    showFishModal: false,
+    showCastle: false,
+    total: 6000,
+    prices: {
+      shed: 1000,
+      pond: 500,
+      castle: 5000
+    },
+    arts: {
+      fishing: 0
+    }
+  },
+  methods: {}
+});
